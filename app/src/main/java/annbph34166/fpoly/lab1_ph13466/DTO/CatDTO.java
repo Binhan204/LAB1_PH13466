43454036a3706605e7931fb9cfbc549121654bd5
NGUYENBINHAN\DELL <annbph34166@fpt.edu.vn>
2025-11-10 11:00:01 +0700

    An commit

---
+++ app/src/main/java/annbph34166/fpoly/lab1_ph13466/DTO/CatDTO.java
@@ -0,0 +1,22 @@
+package annbph34166.fpoly.lab1_ph13466.DTO;
+
+public class CatDTO {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/annbph34166/fpoly/lab1_ph13466/DTO/ProductDTO.java
@@ -0,0 +1,41 @@
+package annbph34166.fpoly.lab1_ph13466.DTO;
+
+public class ProductDTO {
+    int id;
+    String name;
+    int price;    // price NUMBER NOT NULL DEFAULT 0
+    int id_cat;   // khóa ngoại -> tb_cat(id)
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getId_cat() {
+        return id_cat;
+    }
+
+    public void setId_cat(int id_cat) {
+        this.id_cat = id_cat;
+    }
+}
+
